f4e023bd499f67844fee4dfdb39b9f589a3a93f6
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2021-10-17 13:46:29 +0300

    Update Loader.js

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+export default class Loader extends Component {
+    render() {
+        return (
+            <div className="Ids-ellipsis">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        );
+    }
+}
